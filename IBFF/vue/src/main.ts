b26427d235c2be9395f8709cab00df79b2b6d945
latarniakonto <wisniewskikq27@gmail.com>
2022-11-01 17:18:54 +0100

    Add bootstrap icons

--- IBFF/vue/src/main.ts
+++ IBFF/vue/src/main.ts
@@ -4,7 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 loadFonts();
 
