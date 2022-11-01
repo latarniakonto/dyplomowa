3c715ebfba6016c08675449c6b20c7affe1cb4b8
latarniakonto <wisniewskikq27@gmail.com>
2022-11-01 18:27:12 +0100

    Add portfolio dashboard component

--- IBFF/vue/src/components/AppContent.vue
+++ IBFF/vue/src/components/AppContent.vue
@@ -1,10 +1,12 @@
 <template>
   <v-container color="white">
     <v-row no-gutters align="start">
-      <v-col cols="2">
-        <v-sheet height="450" rounded class="ma-3 pa-3"> </v-sheet>
+      <v-col cols="3">
+        <v-sheet height="450" rounded class="ma-3 pa-3">
+          <portfolio-dashboard/>
+        </v-sheet>
       </v-col>
-      <v-col cols="10">
+      <v-col cols="9">
         <v-sheet height="700" rounded class="ma-3 pa-3"> </v-sheet>
       </v-col>
     </v-row>
@@ -13,7 +15,22 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import PortfolioDashboard from "./PortfolioDashboard.vue"
+
 export default defineComponent({
   name: "AppContent",
+
+  components: {
+    PortfolioDashboard
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
 });
 </script>
+
+<style>
+</style>
