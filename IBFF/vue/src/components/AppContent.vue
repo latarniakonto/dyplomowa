c70578c7d14a038780b444e07e559aa7af6c0c7b
latarniakonto <wisniewskikq27@gmail.com>
2022-11-08 16:39:58 +0100

    Add simple example table to app content

--- IBFF/vue/src/components/AppContent.vue
+++ IBFF/vue/src/components/AppContent.vue
@@ -7,7 +7,107 @@
         </v-sheet>
       </v-col>
       <v-col cols="9">
-        <v-sheet height="700" rounded class="ma-3 pa-3"> </v-sheet>
+        <v-sheet height="700" rounded class="ma-3 pa-3">
+          <div class="table-responsive">
+
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Ticker</th>
+                  <th scope="col">Total</th>
+                  <th scope="col">Average Price</th>
+                  <th scope="col">Initial Weight</th>
+                  <th scope="col">Current Price</th>
+                  <th scope="col">Profit/Loss</th>
+                  <th scope="col">Current Weight</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>$ITEM1</td>
+                  <td>93</td>
+                  <td>$</td>
+                  <td>22.68%</td>
+                  <td>$</td>
+                  <td>-16.42%</td>
+                  <td>17.42%</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>$ITEM2</td>
+                  <td>150</td>
+                  <td>$</td>
+                  <td>20.28%</td>
+                  <td>$</td>
+                  <td>-12.01%</td>
+                  <td>16.41%</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>$ITEM3</td>
+                  <td>86</td>
+                  <td>$</td>
+                  <td>5.62%</td>
+                  <td>$</td>
+                  <td>-16.84%</td>
+                  <td>4.3%</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>$ITEM4</td>
+                  <td>55</td>
+                  <td>$</td>
+                  <td>16.87%</td>
+                  <td>$</td>
+                  <td>1.91%</td>
+                  <td>15.82%</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>$ITEM5</td>
+                  <td>84</td>
+                  <td>$</td>
+                  <td>5.15%</td>
+                  <td>$</td>
+                  <td>24.72%</td>
+                  <td>5.91%</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>$ITEM6</td>
+                  <td>80</td>
+                  <td>$</td>
+                  <td>8.08%</td>
+                  <td>$</td>
+                  <td>-16.42%</td>
+                  <td>6.76%</td>
+                </tr>                
+                <tr>
+                  <th scope="row">7</th>
+                  <td>$ITEM7</td>
+                  <td>9</td>
+                  <td>$</td>
+                  <td>1.75%</td>
+                  <td>$</td>
+                  <td>39.53%</td>
+                  <td>2.25%</td>
+                </tr>
+                <tr>
+                  <th scope="row">8</th>
+                  <td>$ITEM8</td>
+                  <td>350</td>
+                  <td>$</td>
+                  <td>19.57%</td>
+                  <td>$</td>
+                  <td>70.15%</td>
+                  <td>30.78%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </v-sheet>
       </v-col>
     </v-row>
   </v-container>
@@ -33,4 +133,9 @@ export default defineComponent({
 </script>
 
 <style>
+tbody tr {
+   line-height: 30px;
+   min-height: 30px;
+   height: 30px;
+}
 </style>
