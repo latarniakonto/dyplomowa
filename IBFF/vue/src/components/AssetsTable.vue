821d91a05b97e6fa2f78c29da0543b56629d6971
latarniakonto <wisniewskikq27@gmail.com>
2022-11-11 13:53:55 +0100

    Add pagination to assets table component

--- IBFF/vue/src/components/AssetsTable.vue
+++ IBFF/vue/src/components/AssetsTable.vue
@@ -15,85 +15,15 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>$ITEM1</td>
-            <td>93</td>
-            <td>$</td>
-            <td>22.68%</td>
-            <td>$</td>
-            <td>-16.42%</td>
-            <td>17.42%</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>$ITEM2</td>
-            <td>150</td>
-            <td>$</td>
-            <td>20.28%</td>
-            <td>$</td>
-            <td>-12.01%</td>
-            <td>16.41%</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>$ITEM3</td>
-            <td>86</td>
-            <td>$</td>
-            <td>5.62%</td>
-            <td>$</td>
-            <td>-16.84%</td>
-            <td>4.3%</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>$ITEM4</td>
-            <td>55</td>
-            <td>$</td>
-            <td>16.87%</td>
-            <td>$</td>
-            <td>1.91%</td>
-            <td>15.82%</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>$ITEM5</td>
-            <td>84</td>
-            <td>$</td>
-            <td>5.15%</td>
-            <td>$</td>
-            <td>24.72%</td>
-            <td>5.91%</td>
-          </tr>
-          <tr>
-            <th scope="row">6</th>
-            <td>$ITEM6</td>
-            <td>80</td>
-            <td>$</td>
-            <td>8.08%</td>
-            <td>$</td>
-            <td>-16.42%</td>
-            <td>6.76%</td>
-          </tr>
-          <tr>
-            <th scope="row">7</th>
-            <td>$ITEM7</td>
-            <td>9</td>
-            <td>$</td>
-            <td>1.75%</td>
-            <td>$</td>
-            <td>39.53%</td>
-            <td>2.25%</td>
-          </tr>
-          <tr>
-            <th scope="row">8</th>
-            <td>$ITEM8</td>
-            <td>350</td>
-            <td>$</td>
-            <td>19.57%</td>
-            <td>$</td>
-            <td>70.15%</td>
-            <td>30.78%</td>
+          <tr v-for="(stock, idx) in stocksToShow" :key="stock.ticker">
+            <th scope="row">{{ firstStockNumber + idx }}</th>
+            <td>{{ stock.ticker }}</td>
+            <td>{{ stock.total }}</td>
+            <td>{{ stock.buyPrice }}</td>
+            <td>{{ stock.initialWeight }}</td>
+            <td>{{ stock.currentPrice }}</td>
+            <td>{{ stock.gain }}</td>
+            <td>{{ stock.currentWeight }}</td>
           </tr>
         </tbody>
       </table>
@@ -101,18 +31,30 @@
     <div class="table-tools">
       <div class="container-select">
         <p class="text-select mt-3 mr-6">Rows per page:</p>
-        <select class="form-select form-select-md mt-1 mr-6">
+        <select
+          @change="refreshStocksToShow()"
+          v-model="rowsPerPage"
+          class="form-select form-select-md mt-1 mr-6"
+        >
           <option value="5" selected>5</option>
           <option value="10">10</option>
         </select>
-        <p class="text-select mt-3 mr-6">1-5 of 8</p>
+        <p v-if="lastStockNumber < stocks.length" class="text-select mt-3 mr-6">{{ firstStockNumber }}-{{ lastStockNumber }} of {{ stocks.length }}</p>
+        <p v-else class="text-select mt-3 mr-6">{{ firstStockNumber }}-{{ stocks.length }} of {{ stocks.length }}</p>
         <button
           id="app-content"
           class="btn btn-outline-secondary btn-icon mr-6"
+          :class="{ disabled: currentPagination === 1 }"
+          @click="refreshStocksToShow(--currentPagination)"
         >
           <i class="bi bi-chevron-left"></i>
         </button>
-        <button id="app-content" class="btn btn-outline-secondary btn-icon">
+        <button
+          id="app-content"
+          class="btn btn-outline-secondary btn-icon"
+          :class="{ disabled: lastStockNumber > stocks.length }"
+          @click="refreshStocksToShow(++currentPagination)"
+        >
           <i class="bi bi-chevron-right"></i>
         </button>
       </div>
@@ -133,8 +75,105 @@ export default defineComponent({
 
   data() {
     return {
-      //
+      rowsPerPage: "5" as String,
+      currentPagination: 1 as number,
+      firstStockNumber: 1 as number,
+      lastStockNumber: 5 as number,
+      stocksToShow: [] as Array<Object>,
+      stocks: [
+        {
+          ticker: "$ITEM1" as String,
+          total: "93" as String,
+          buyPrice: "$" as String,
+          initialWeight: "22.68%" as String,
+          currentPrice: "$" as String,
+          gain: "-16.42%" as String,
+          currentWeight: "17.42%" as String,
+        },
+        {
+          ticker: "$ITEM2" as String,
+          total: "150" as String,
+          buyPrice: "$" as String,
+          initialWeight: "20.28%" as String,
+          currentPrice: "$" as String,
+          gain: "-12.01%" as String,
+          currentWeight: "16.41%" as String,
+        },
+        {
+          ticker: "$ITEM3" as String,
+          total: "86" as String,
+          buyPrice: "$" as String,
+          initialWeight: "5.62%" as String,
+          currentPrice: "$" as String,
+          gain: "-16.84%" as String,
+          currentWeight: "4.3%" as String,
+        },
+        {
+          ticker: "$ITEM4" as String,
+          total: "55" as String,
+          buyPrice: "$" as String,
+          initialWeight: "16.87%" as String,
+          currentPrice: "$" as String,
+          gain: "1.91%" as String,
+          currentWeight: "15.82%" as String,
+        },
+        {
+          ticker: "$ITEM5" as String,
+          total: "84" as String,
+          buyPrice: "$" as String,
+          initialWeight: "5.15%" as String,
+          currentPrice: "$" as String,
+          gain: "24.72%" as String,
+          currentWeight: "5.91%" as String,
+        },
+        {
+          ticker: "$ITEM6" as String,
+          total: "80" as String,
+          buyPrice: "$" as String,
+          initialWeight: "8.08%" as String,
+          currentPrice: "$" as String,
+          gain: "-16.42%" as String,
+          currentWeight: "6.76%" as String,
+        },
+        {
+          ticker: "$ITEM7" as String,
+          total: "9" as String,
+          buyPrice: "$" as String,
+          initialWeight: "1.75%" as String,
+          currentPrice: "$" as String,
+          gain: "39.53%" as String,
+          currentWeight: "2.25%" as String,
+        },
+        {
+          ticker: "$ITEM1" as String,
+          total: "350" as String,
+          buyPrice: "$" as String,
+          initialWeight: "19.57%" as String,
+          currentPrice: "$" as String,
+          gain: "70.15%" as String,
+          currentWeight: "30.78%" as String,
+        },
+      ] as Array<Object>,
     };
+  },
+
+  methods: {
+    refreshStocksToShow: function (pagination: number = 1) {
+      this.currentPagination = pagination;
+      this.stocksToShow = [];
+      let index = Number(this.rowsPerPage) * (pagination - 1);
+
+      this.firstStockNumber = index + 1;
+      this.lastStockNumber = index + Number(this.rowsPerPage);            
+
+      for (let first = index; index < this.stocks.length && index < first + Number(this.rowsPerPage); index++) {
+        this.stocksToShow.push(this.stocks[index]);
+      }
+    },
+  },
+
+  created() {
+    this.refreshStocksToShow();
   },
 });
 </script>
