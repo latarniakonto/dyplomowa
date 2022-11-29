4a0784150d1641304c264576344b5facb2d49dcf
latarniakonto <wisniewskikq27@gmail.com>
2022-11-29 12:42:01 +0100

    Change app content wireframe and add navigation

--- IBFF/vue/src/components/AppContent.vue
+++ IBFF/vue/src/components/AppContent.vue
@@ -1,40 +1,44 @@
 <template>
-  <v-container color="white">
-    <v-row no-gutters align="start">
-      <v-col cols="3">
-        <v-sheet height="470" rounded class="ma-3 pa-3">
-          <portfolio-dashboard/>
-        </v-sheet>
-      </v-col>
-      <v-col cols="9">
-        <v-sheet height="700" rounded class="ma-3 pa-3">
-          <assets-table/>
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="container app-content mt-3">
+    <TabMenu :model="items" class="mb-3"/>
+    <AssetsTable/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import PortfolioDashboard from "./PortfolioDashboard.vue";
 import AssetsTable from "./AssetsTable.vue";
+import TabMenu from "primevue/tabmenu";
 
 export default defineComponent({
   name: "AppContent",
 
   components: {
     PortfolioDashboard,
-    AssetsTable
+    AssetsTable,
+    TabMenu,
   },
 
   data() {
     return {
-      //
+      items: [
+        { label: "Home", icon: "bi bi-house" },
+        { label: "Assets", icon: "bi bi-wallet2" },
+        { label: "Transactions", icon: "bi bi-cash-coin" },
+        { label: "Operations", icon: "bi bi-boxes" },
+      ],
     };
   },
 });
 </script>
 
 <style>
+.app-content {
+  display: block;
+  height: 47rem;
+  background: white;
+  padding: 1rem;
+  border-radius: 4px;
+}
 </style>
