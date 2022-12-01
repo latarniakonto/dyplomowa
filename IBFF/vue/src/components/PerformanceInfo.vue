9ac37830fb1105b0a3e6463bc121452974a9aba7
latarniakonto <wisniewskikq27@gmail.com>
2022-12-01 16:02:25 +0100

    Create separate performance info component

---
+++ IBFF/vue/src/components/PerformanceInfo.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="performance">
+    <h5 class="mb-2 title">Performance</h5>
+    <div
+      class="tile mb-2"
+      :class="{ 'tile-active': performanceInfoTiles[0] }"
+      @click="togglePerformanceInfoTile(0)"
+    >
+      <div class="mb-2">
+        <span class="font-medium text-xl">Annual Gain</span>
+      </div>
+      <div>
+        <span class="text-bluegray-900 text-2xl">47000</span>
+      </div>
+    </div>
+    <div
+      class="tile mb-2"
+      :class="{ 'tile-active': performanceInfoTiles[1] }"
+      @click="togglePerformanceInfoTile(1)"
+    >
+      <div class="mb-2">
+        <span class="font-medium text-xl">Annual Yield</span>
+      </div>
+      <div>
+        <span class="text-bluegray-900 text-2xl">156.67%</span>
+      </div>
+    </div>
+    <div
+      class="tile mb-2"
+      :class="{ 'tile-active': performanceInfoTiles[2] }"
+      @click="togglePerformanceInfoTile(2)"
+    >
+      <div class="mb-2">
+        <span class="font-medium text-xl">Total Annual Dividends</span>
+      </div>
+      <div>
+        <span class="text-bluegray-900 text-2xl">1200</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PerformanceInfo",
+
+  components: {},
+
+  data() {
+    return {
+      performanceInfoTiles: [false, false, false] as Array<Boolean>,
+    };
+  },
+
+  methods: {
+    togglePerformanceInfoTile(index: number) {
+      for (let i = 0; i < this.performanceInfoTiles.length; i++) {
+        if (i == index) {
+          continue;
+        }
+        this.performanceInfoTiles[i] = false;
+      }
+
+      this.performanceInfoTiles[index] = !this.performanceInfoTiles[index];
+    },
+  },
+});
+</script>
+
+<style>
+.tile {
+  cursor: pointer;
+  height: 7rem;
+  width: 18.666rem;
+  padding: 1rem;
+  border-radius: 4px;
+  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 1px 3px 0 rgba(92, 76, 76, 0.12);
+  background-color: #f6f6f6;
+  border-bottom-style: solid;
+  border-bottom-width: 0.1rem;
+  border-bottom-color: #a4a4a4;
+  display: inline-block;
+}
+
+.tile.tile-active {
+  background-color: #ededed;
+  box-shadow: inset 0px 0px 6px #d3d3d3;
+}
+
+.tile .p-inputnumber .p-inputnumber-input {
+  width: 7rem;
+  padding: 0.5rem;
+}
+
+.tile .p-inputnumber.input-blocked .p-inputnumber-input {
+  border: 1px solid rgba(0, 0, 0, 0.38);
+  cursor: default;
+  background: rgba(255, 255, 255, 0.38);
+  color: rgba(0, 0, 0, 0.38);
+}
+
+.tile .p-inputnumber.input-blocked .p-inputnumber-input:focus {
+  border: 1px solid rgba(0, 0, 0, 0.38);
+  background: rgba(255, 255, 255, 0.38);
+  color: rgba(0, 0, 0, 0.38);
+  box-shadow: none;
+}
+
+.tile .p-inputgroup-addon {
+  width: 3rem;
+  padding: 0.5rem;
+  background-color: #ededed;
+}
+
+.tile:hover {
+  background-color: #ededed;
+}
+
+.performance {
+  display: inline-block;
+  width: 18.66rem;
+}
+
+.performance .title {
+  border-bottom: solid 2px;
+  border-bottom-color: #a4a4a4;
+  padding-left: 0.5rem;
+  padding-bottom: 0.5rem;
+}
+</style>
--- IBFF/vue/src/components/PortfolioDashboard.vue
+++ IBFF/vue/src/components/PortfolioDashboard.vue
@@ -10,45 +10,7 @@
         <ChartsInfo />
       </div>
       <div class="d-inline-block align-top">
-        <div class="performance">
-          <h5 class="mb-2 title">Performance</h5>
-          <div
-            class="tile mb-2"
-            :class="{ 'tile-active': assetInfoTiles[6] }"
-            @click="toggleAssetInfoTile(6)"
-          >
-            <div class="mb-2">
-              <span class="font-medium text-xl">Annual Gain</span>
-            </div>
-            <div>
-              <span class="text-bluegray-900 text-2xl">47000</span>
-            </div>
-          </div>
-          <div
-            class="tile mb-2"
-            :class="{ 'tile-active': assetInfoTiles[5] }"
-            @click="toggleAssetInfoTile(5)"
-          >
-            <div class="mb-2">
-              <span class="font-medium text-xl">Annual Yield</span>
-            </div>
-            <div>
-              <span class="text-bluegray-900 text-2xl">156.67%</span>
-            </div>
-          </div>
-          <div
-            class="tile mb-2"
-            :class="{ 'tile-active': assetInfoTiles[7] }"
-            @click="toggleAssetInfoTile(7)"
-          >
-            <div class="mb-2">
-              <span class="font-medium text-xl">Total Annual Dividends</span>
-            </div>
-            <div>
-              <span class="text-bluegray-900 text-2xl">1200</span>
-            </div>
-          </div>
-        </div>
+        <PerformanceInfo />
       </div>
     </div>
   </div>
@@ -59,6 +21,7 @@ import { defineComponent } from "vue";
 import ResourcesInfo from "./ResourcesInfo.vue";
 import FeesInfo from "./FeesInfo.vue";
 import ChartsInfo from "./ChartsInfo.vue";
+import PerformanceInfo from "./PerformanceInfo.vue";
 
 export default defineComponent({
   name: "PortfolioDashboard",
@@ -67,6 +30,7 @@ export default defineComponent({
     ResourcesInfo,
     FeesInfo,
     ChartsInfo,
+    PerformanceInfo
   },
 
   data() {
@@ -77,97 +41,15 @@ export default defineComponent({
         { label: "Transactions", icon: "bi bi-cash-coin" },
         { label: "Operations", icon: "bi bi-boxes" },
       ],
-      assetInfoTiles: [
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-      ] as Array<Boolean>,
     };
   },
 
   methods: {
-    toggleAssetInfoTile(index: number) {
-      for (let i = 0; i < this.assetInfoTiles.length; i++) {
-        if (i == index) {
-          continue;
-        }
-        this.assetInfoTiles[i] = false;
-      }
-
-      this.assetInfoTiles[index] = !this.assetInfoTiles[index];
-    },
   },
 });
 </script>
 
 <style>
-.tile {
-  cursor: pointer;
-  height: 7rem;
-  width: 18.666rem;
-  padding: 1rem;
-  border-radius: 4px;
-  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14),
-    0 1px 3px 0 rgba(92, 76, 76, 0.12);
-  background-color: #f6f6f6;
-  border-bottom-style: solid;
-  border-bottom-width: 0.1rem;
-  border-bottom-color: #a4a4a4;
-  display: inline-block;
-}
-
-.tile.tile-active {
-  background-color: #ededed;
-  box-shadow: inset 0px 0px 6px #d3d3d3;
-}
-
-.tile .p-inputnumber .p-inputnumber-input {
-  width: 7rem;
-  padding: 0.5rem;
-}
-
-.tile .p-inputnumber.input-blocked .p-inputnumber-input {
-  border: 1px solid rgba(0, 0, 0, 0.38);
-  cursor: default;
-  background: rgba(255, 255, 255, 0.38);
-  color: rgba(0, 0, 0, 0.38);
-}
-
-.tile .p-inputnumber.input-blocked .p-inputnumber-input:focus {
-  border: 1px solid rgba(0, 0, 0, 0.38);
-  background: rgba(255, 255, 255, 0.38);
-  color: rgba(0, 0, 0, 0.38);
-  box-shadow: none;
-}
-
-.tile .p-inputgroup-addon {
-  width: 3rem;
-  padding: 0.5rem;
-  background-color: #ededed;
-}
-
-.tile:hover {
-  background-color: #ededed;
-}
-
-.performance {
-  display: inline-block;
-  width: 18.66rem;
-}
-
-.performance .title {
-  border-bottom: solid 2px;
-  border-bottom-color: #a4a4a4;
-  padding-left: 0.5rem;
-  padding-bottom: 0.5rem;
-}
-
 .portoflio-dashboard {
   display: flex;
 }
