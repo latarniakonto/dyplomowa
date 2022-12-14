a09bcd0c26ec28227bc33cd5c7fc13b60412dfc7
latarniakonto <wisniewskikq27@gmail.com>
2022-12-14 10:50:45 +0100

    Filter user portfolios

---
+++ IBFF/portfolios/api/filters.py
@@ -0,0 +1,6 @@
+from rest_framework.filters import BaseFilterBackend
+
+
+class IsOwnerFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(owner=request.user)
--- IBFF/portfolios/api/views.py
+++ IBFF/portfolios/api/views.py
@@ -3,6 +3,7 @@ from portfolios.models import Portfolio
 from portfolios.api.serializers import PortfolioSerializer
 from rest_framework.permissions import IsAuthenticated
 from portfolios.api.permissions import IsPortfolioOwner
+from portfolios.api.filters import IsOwnerFilter
 
 
 class PortfolioViewSet(viewsets.ModelViewSet):
@@ -10,6 +11,7 @@ class PortfolioViewSet(viewsets.ModelViewSet):
     serializer_class = PortfolioSerializer
     lookup_field = 'slug'
     permission_classes = [IsAuthenticated, IsPortfolioOwner]
+    filter_backends = [IsOwnerFilter]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
