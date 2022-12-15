fe62048b4e796058dbaeb28c4e90a4a8d8c54266
latarniakonto <wisniewskikq27@gmail.com>
2022-12-15 16:32:14 +0100

    Show specific portfolio transactions

--- IBFF/portfolios/api/urls.py
+++ IBFF/portfolios/api/urls.py
@@ -1,11 +1,18 @@
 from django.urls import include, path
+from django.contrib.auth.decorators import permission_required
 from rest_framework.routers import DefaultRouter
-from portfolios.api import views
+from portfolios.api import views as portfolios_views
+from transactions.api import views as transactions_views
 
 
 router = DefaultRouter()
-router.register(r"portfolios", views.PortfolioViewSet)
+router.register(r'portfolios', portfolios_views.PortfolioViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path('', include(router.urls)),
+    path(
+        'portfolios/<slug:slug>/transactions/',
+        transactions_views.TransactionViewSet.as_view({'get': 'list'}),
+        name='transactions-list',
+    ),
 ]
--- IBFF/transactions/api/views.py
+++ IBFF/transactions/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from transactions.models import Transaction
+from portfolios.models import Portfolio
 from transactions.api.serializers import TransactionSerializer
 from rest_framework.permissions import IsAuthenticated
 from transactions.api.permissions import IsContainedInOwnedPortfolios
 from transactions.api.filters import OwnedPortfoliosFilter
-
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
@@ -15,3 +17,13 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save()
+
+    def list(self, request, slug=None):
+        if slug is None:
+            return super().list(request)
+        else:
+            user_portfolio = get_object_or_404(Portfolio, owner=request.user, slug=slug)
+            queryset = user_portfolio.transactions
+
+        serializer = TransactionSerializer(queryset, many=True)
+        return Response(serializer.data)
