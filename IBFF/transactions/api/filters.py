eace976cde1f6a39768918ea5d5c153592a00ad2
latarniakonto <wisniewskikq27@gmail.com>
2022-12-26 17:31:52 +0100

    Filter owned portfolio transactions

--- IBFF/transactions/api/filters.py
+++ IBFF/transactions/api/filters.py
@@ -6,6 +6,9 @@ from django.db.models import Q
 class OwnedPortfoliosFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         user_portfolios = Portfolio.objects.filter(owner=request.user)
+        if len(user_portfolios) == 0:
+            return []
+
         query = Q()
         for user_portfolio in user_portfolios:
             query |= Q(portfolio=user_portfolio.id)
