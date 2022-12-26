4979bfc222fa9baa6e88a3d6dfc8e4ddcb572ea3
latarniakonto <wisniewskikq27@gmail.com>
2022-12-26 18:15:39 +0100

    Show specific asset transactions

--- IBFF/assets/api/urls.py
+++ IBFF/assets/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from assets.api import views as assets_views
+from transactions.api import views as transactions_views
 
 
 router = DefaultRouter()
@@ -8,4 +9,9 @@ router.register(r'assets', assets_views.AssetViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path(
+        'assets/<slug:a_slug>/transactions/',
+        transactions_views.TransactionViewSet.as_view({'get': 'list'}),
+        name='transactions-list',
+    ),
 ]
--- IBFF/transactions/api/views.py
+++ IBFF/transactions/api/views.py
@@ -7,6 +7,7 @@ from transactions.api.permissions import IsContainedInOwnedPortfolios
 from transactions.api.filters import OwnedPortfoliosFilter
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from assets.models import Asset
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
@@ -22,13 +23,17 @@ class TransactionViewSet(viewsets.ModelViewSet):
         )
         serializer.save(portfolio=portfolio)
 
-    def list(self, request, p_slug=None):
-        if p_slug is None:
+    def list(self, request, p_slug=None, a_slug=None):
+        if p_slug is None and a_slug is None:
             return super().list(request)
 
         elif p_slug:
             portfolio = get_object_or_404(Portfolio, slug=p_slug, owner=request.user)
             queryset = portfolio.transactions
+
+        elif a_slug:
+            asset = get_object_or_404(Asset, slug=a_slug, portfolio__owner=request.user)
+            queryset = asset.portfolio.transactions.filter(ticker=asset.ticker)
 
         serializer = TransactionSerializer(queryset, many=True)
         return Response(serializer.data)
