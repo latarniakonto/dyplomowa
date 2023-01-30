bfe47548691cb30f48845815c83ceaf480e15a33
latarniakonto <wisniewskikq27@gmail.com>
2023-01-30 19:02:42 +0100

    Remove about view

--- IBFF/vue/src/router/index.ts
+++ IBFF/vue/src/router/index.ts
@@ -63,15 +63,6 @@ const routes: Array<RouteRecordRaw> = [
       return { path: "/snapshots", query: { slug: to.params.slug }};
     }
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
 ];
 
 const router = createRouter({
