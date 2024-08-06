3c12c928c453f762f7996556076f03b9c2a3f408
latarniakonto <wisniewskikq27@gmail.com>
2024-08-06 12:03:15 +0200

    Enable HMR for razor pages

--- src/IBFF/Program.cs
+++ src/IBFF/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Enable HMR for razor pages
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
